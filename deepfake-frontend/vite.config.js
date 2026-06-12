450240d1fad3ed9d87faf7643254e1ab4ded3282
Yallaling Reddy <yallalingreddy18@gmail.com>
2026-06-12 14:55:26 +0530

    Updated frontend and backend for deployment

--- deepfake-frontend/vite.config.js
+++ deepfake-frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    // This forces Vite to ALWAYS use the same single copy of React
+    dedupe: ['react', 'react-dom'],
+  },
 })
