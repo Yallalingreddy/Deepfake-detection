d5507955bbf40fac11099726bbf9bc62ffa26e77
Yallaling Reddy <yallalingreddy18@gmail.com>
2026-06-12 16:19:56 +0530

    Connected frontend to Render backend

--- deepfake-frontend/src/App.jsx
+++ deepfake-frontend/src/App.jsx
@@ -877,7 +877,7 @@ export default function App() {
 
     try {
       setLoading(true);
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://mobile-srm-deepfake-detector.onrender.com/predict", {
         method: "POST",
         body: formData,
       });
